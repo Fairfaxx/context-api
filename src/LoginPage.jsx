2d01f075fbab070ec3d6879f3b25fb18b1f93205
Fede <53632624+Fairfaxx@users.noreply.github.com>
2023-01-25 18:39:11 +0100

    small app to show the power of Context API and the new version of react-router-dom V6 (#2)
    Co-authored-by: FefeLemaire <federico.lemaire@alloy.digital>

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -2,12 +2,26 @@ import { useContext } from 'react';
 import { UserContext } from './context/UserContext';
 
 function LoginPage() {
-  const user = useContext(UserContext);
-  console.log(user.user);
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <div className="App">
       <h1>LoginPage</h1>
       <hr />
+      <button
+        onClick={() =>
+          setUser({
+            id: 123,
+            name: 'John',
+            lastName: 'Doe',
+            email: 'john@example.com',
+          })
+        }
+        className="btn btn-outline-primary"
+      >
+        Set User
+      </button>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
     </div>
   );
 }
